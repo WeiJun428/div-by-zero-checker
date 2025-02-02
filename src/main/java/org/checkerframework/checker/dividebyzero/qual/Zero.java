8661269196c3161a8c7b1adbecceb894833080d5
Wei Jun Tan <wj428@uw.edu>
2025-02-01 18:46:37 -0800

    Define lattice

---
+++ src/main/java/org/checkerframework/checker/dividebyzero/qual/Zero.java
@@ -0,0 +1,8 @@
+package org.checkerframework.checker.dividebyzero.qual;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+import org.checkerframework.framework.qual.*;
+
+@Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
+@SubtypeOf({Top.class})
+public @interface Zero {}
